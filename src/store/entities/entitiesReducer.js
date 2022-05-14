b57fca0564772fb964a67c77ff1692e1e6105ddf
andrerodrigues <andrerodrigues29101992@gmail.com>
2022-05-14 19:48:04 +0100

    Updates

--- src/store/entities/entitiesReducer.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-import gameReducer from "./game";
-
-export default combineReducers({
-  game: gameReducer
-});
--- src/store/rootReducer.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-import entitiesReducer from "./entities/entitiesReducer";
-
-export default combineReducers({
-  entities: entitiesReducer
-});
